eea399ccfcf2d8a2dfacb8da080e993b41816145
Matthew O'Mara <tuh39725@temple.edu>
2022-02-15 21:24:05 -0500

    add color adapter to main activity

--- app/src/main/java/edu/temple/myapplication/ColorActivity.kt
+++ app/src/main/java/edu/temple/myapplication/ColorActivity.kt
@@ -2,10 +2,33 @@ package edu.temple.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 
 class ColorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val colors = arrayOf("Gray", "Magenta", "Yellow", "Red", "Purple")
+
+        val adapter = ColorAdapter(this, colors)
+
+        spinner.adapter = adapter
+
+        val eventListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
+
+        spinner.onItemSelectedListener = eventListener
+
     }
 }
